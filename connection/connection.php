f56d5882cbae525c8d31f0a4da1b5e14a044b3db
sportapora <henrysalim22@gmail.com>
2024-10-10 22:40:50 +0700

    add connection to gitignore

--- connection/connection.php
+++ connection/connection.php
@@ -1,8 +1,3 @@
 <?php
-// password menyesuaikan
 
-// Henry
-// $connection = new PDO("mysql:host=localhost;dbname=uts_webprog", "root", "wian0801");
-
-// Wian
-$connection = new PDO("mysql:host=localhost;dbname=uts_webprog", "root", "wian0801");
+$connection = new PDO("mysql:host=localhost;dbname=uts_webprog", "root", "SQLP@ss98");
