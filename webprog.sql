092e1a19628bcb06b6cf6c2478fc65bbde01753c
sportapora <henrysalim22@gmail.com>
2024-10-24 15:15:06 +0700

    fix sql

--- webprog.sql
+++ webprog.sql
@@ -51,8 +51,7 @@ TABLES `events` WRITE;
 UNLOCK
 TABLES;
 
-
---
+       --
 -- Table structure for table `users`
 --
 
@@ -71,8 +70,22 @@ CREATE TABLE `users`
     PRIMARY KEY (`id`),
     UNIQUE KEY `username` (`username`),
     UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK
+TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users`
+VALUES (1, 'Admin Festivo', 'festivo@festivo.com', '$2y$10$9yMJqheJ8Dh.7Xlk8t1xQul5g.nnSD5FUF4wwU2cdBjpyUEv1e.Ia',
+        'admin', '2024-10-24 08:13:30', '2024-10-24 08:13:30');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK
+TABLES;
 
 --
 -- Table structure for table `events_users`
@@ -107,16 +120,6 @@ TABLES `events_users` WRITE;
 UNLOCK
 TABLES;
 
---
--- Dumping data for table `users`
---
-
-LOCK
-TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK
-TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -127,4 +130,4 @@ TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-24 14:15:24
+-- Dump completed on 2024-10-24 15:13:54
